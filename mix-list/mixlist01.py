6bc2df1845ca60ddd2e2bd7a1ab0751f9b1c2557
tranandy-919 <andy.tran@tlgcohort.com>
2024-01-09 23:08:12 +0000

    up to lab 30

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+# prints first item in list
+print("The first item in the list (IP): " + my_list[0] )
+#prints 2nd item in list, cast to str because its a string 
+print("The 2nd item in the list (port): " + str(my_list[1]) )
+#pirnts 3rd item in list 
+print("The last item in the list (state): " + my_list[2] )
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print(iplist[3], iplist[4])
